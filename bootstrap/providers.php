89c6f843013d642ff8eec22466d0ed0a2f944e91
vroa74 <33233830+vroa74@users.noreply.github.com>
2026-07-25 08:56:59 -0600

    1

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,8 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\LivewireServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
-    App\Providers\LivewireServiceProvider::class,
     Barryvdh\DomPDF\ServiceProvider::class,
 ];
