358ba253624a34a79e56b675930c27f9ecc1b874
vroa74 <33233830+vroa74@users.noreply.github.com>
2026-07-25 07:57:45 -0600

    Initial commit

---
+++ app/Providers/LivewireServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
+use App\Livewire\Profile\UpdateProfileInformationForm;
+use App\Livewire\Service\Edit as ServiceEdit;
+
+class LivewireServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        // Registrar nuestros componentes personalizados
+        Livewire::component('profile.update-profile-information-form', UpdateProfileInformationForm::class);
+        Livewire::component('service.edit', ServiceEdit::class);
+    }
+}
